059908f9f731fa54f8dbd380a17f626eca010ae0
rafay <rafaykakar1029@gmail.com>
2025-04-14 00:34:08 +0200

    cpkllk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,7 +96,7 @@ android {
             property("sonar.tests", "./src/test")
             property("sonar.host.url", "https://sonarcloud.io")
             property("sonar.projectName", "Note-MVVM-Application")
-            property("sonar.projectKey", "RafayKakar_Note-MVVM-Application")
+            property("sonar.projectKey", "RafayKakar-Note-MVVM-Application")
             property("sonar.organization", "rafaykakar")
             property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
         }

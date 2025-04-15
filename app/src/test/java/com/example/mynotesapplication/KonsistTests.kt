e4cb747687e3a16ac5f978ff2e74a9b4b103f35f
RafayKakar <rafaykakar1029@gmail.com>
2025-04-15 13:15:15 +0500

    konsist test success

--- app/src/test/java/com/example/mynotesapplication/KonsistTests.kt
+++ app/src/test/java/com/example/mynotesapplication/KonsistTests.kt
@@ -14,4 +14,52 @@ class KonsistTests {
             .assertTrue { it.name.endsWith("Activity")  }
     }
 
+    @Test
+    fun `activity and fragment classes should have @AndroidEntryPoint annotation`() {
+        Konsist.scopeFromProject()
+            .classes()
+            .filter {
+                it.name.endsWith("Activity") || it.name.endsWith("Fragment")
+            }
+            .assertTrue { clazz ->
+                clazz.hasAnnotationWithName("AndroidEntryPoint")
+            }
+    }
+
+
+    @Test
+    fun `adapter should lie in adapters package and name should end with Adapter`() {
+        Konsist.scopeFromProject()
+            .files
+            .filter {it.resideInPath("..adapters..") }
+            .assertTrue { it.name.endsWith("Adapter")  }
+    }
+
+
+
+    @Test
+    fun `all DAO classes should be interfaces in database package and end with Dao`() {
+        Konsist.scopeFromPackage("..database..")
+            .interfaces()
+            .assertTrue { it.name.endsWith("Dao") }
+    }
+
+
+    @Test
+    fun `repository should lie in repository package and name should end with Respository`() {
+        Konsist.scopeFromProject()
+            .files
+            .filter {it.resideInPath("..repository..") }
+            .assertTrue { it.name.endsWith("Repository")  }
+    }
+
+
+    @Test
+    fun `viewmodel should lie in viewmodels package and name should end with ViewModel`() {
+        Konsist.scopeFromProject()
+            .files
+            .filter {it.resideInPath("..viewmodels..") }
+            .assertTrue { it.name.endsWith("ViewModel")  }
+    }
+
 }
